d234d4753488ae8ae1e7f9211cdd8216d8ededab
[Lampart] Nguyen Van Hoai <van_hoai@lampart-vn.com>
2019-07-02 11:17:22 +0700

    fix config

--- spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/RequestTraceFilter.java
+++ spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/RequestTraceFilter.java
@@ -1,28 +1,58 @@
 package com.sedooe.actuator.config;
 
-import org.springframework.boot.actuate.trace.InMemoryTraceRepository;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.actuate.trace.TraceProperties;
 import org.springframework.boot.actuate.trace.TraceRepository;
 import org.springframework.boot.actuate.trace.WebRequestTraceFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-
 @Component
 public class RequestTraceFilter extends WebRequestTraceFilter {
 
-    private final String[] excludedEndpoints = new String[]{"/css/**", "/js/**", "/trace"};
+    private final String[] excludedEndpoints = new String[] { "/css/**", "/js/**", "/trace" };
 
     RequestTraceFilter(TraceRepository repository, TraceProperties properties) {
         super(repository, properties);
     }
 
     @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        TeeHttpServletResponse teeResponse = new TeeHttpServletResponse(response);
+
+        filterChain.doFilter(request, teeResponse);
+
+        teeResponse.finish();
+
+        request.setAttribute("responseBody", teeResponse.getOutputBuffer());
+
+        super.doFilterInternal(request, teeResponse, filterChain);
+    }
+
+    @Override
+    protected Map<String, Object> getTrace(HttpServletRequest request) {
+        Map<String, Object> trace = super.getTrace(request);
+
+        byte[] outputBuffer = (byte[]) request.getAttribute("responseBody");
+
+        if (outputBuffer != null) {
+            trace.put("responseBody", new String(outputBuffer));
+        }
+
+        return trace;
+    }
+
+    @Override
     protected boolean shouldNotFilter(final HttpServletRequest request) throws ServletException {
-        return Arrays.stream(excludedEndpoints)
-                .anyMatch(e -> new AntPathMatcher().match(e, request.getServletPath()));
+        return Arrays.stream(excludedEndpoints).anyMatch(e -> new AntPathMatcher().match(e, request.getServletPath()));
     }
 }
---
+++ spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/TeeHttpServletResponse.java
@@ -0,0 +1,62 @@
+package com.sedooe.actuator.config;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+public class TeeHttpServletResponse extends HttpServletResponseWrapper {
+
+    TeeServletOutputStream teeServletOutputStream;
+    PrintWriter teeWriter;
+
+    public TeeHttpServletResponse(HttpServletResponse httpServletResponse) {
+        super(httpServletResponse);
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        if (teeServletOutputStream == null) {
+            teeServletOutputStream = new TeeServletOutputStream(this.getResponse());
+        }
+        return teeServletOutputStream;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        if (teeWriter == null) {
+            teeWriter = new PrintWriter(
+                    new OutputStreamWriter(getOutputStream(), this.getResponse().getCharacterEncoding()), true);
+        }
+        return teeWriter;
+    }
+
+    @Override
+    public void flushBuffer() {
+        if (teeWriter != null) {
+            teeWriter.flush();
+        }
+    }
+
+    byte[] getOutputBuffer() {
+        // teeServletOutputStream can be null if the getOutputStream method is never
+        // called.
+        if (teeServletOutputStream != null) {
+            return teeServletOutputStream.getOutputStreamAsByteArray();
+        } else {
+            return null;
+        }
+    }
+
+    void finish() throws IOException {
+        if (teeWriter != null) {
+            teeWriter.close();
+        }
+        if (teeServletOutputStream != null) {
+            teeServletOutputStream.close();
+        }
+    }
+}
