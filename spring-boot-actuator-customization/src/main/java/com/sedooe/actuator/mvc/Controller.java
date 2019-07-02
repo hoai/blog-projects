76ea2d9b3caec402ae2849eb64203952df75efae
[Lampart] Nguyen Van Hoai <van_hoai@lampart-vn.com>
2019-07-02 11:43:54 +0700

    add pom json

---
+++ spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/mvc/Controller.java
@@ -0,0 +1,38 @@
+package com.sedooe.actuator.mvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @RequestMapping(value = "/resource", method = RequestMethod.GET)
+    public Map<String, String> getResource() {
+        Map<String, String> resource = new HashMap<String, String>();
+        resource.put("resource", "here is some resource");
+        return resource;
+    }
+
+    @RequestMapping(value = "/resource", method = RequestMethod.POST)
+    public Map<String, Integer> setResource(@RequestBody Customer customer) {
+        Map<String, Integer> resource = new HashMap<String, Integer>();
+        resource.put("resource", customer.getCustomerId());
+        return resource;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(HttpSession session) {
+        session.invalidate();
+    }
+}
---
+++ spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/mvc/Customer.java
@@ -0,0 +1,13 @@
+package com.sedooe.actuator.mvc;
+
+public class Customer {
+    private Integer customerId;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+}
