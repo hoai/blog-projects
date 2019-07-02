930e5e31828d45f610f952196c21522293b1f01c
[Lampart] Nguyen Van Hoai <van_hoai@lampart-vn.com>
2019-07-02 15:51:23 +0700

    fix log request body

--- spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/RequestTraceFilter.java
+++ spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/RequestTraceFilter.java
@@ -1,6 +1,7 @@
 package com.sedooe.actuator.config;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -14,42 +15,112 @@ import org.springframework.boot.actuate.trace.TraceRepository;
 import org.springframework.boot.actuate.trace.WebRequestTraceFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
 
 @Component
 public class RequestTraceFilter extends WebRequestTraceFilter {
 
     private final String[] excludedEndpoints = new String[] { "/css/**", "/js/**", "/trace" };
+    private static final String RESPONSE_BODY = "resBody";
+    private static final String REQUEST_BODY = "reqBody";
 
     RequestTraceFilter(TraceRepository repository, TraceProperties properties) {
         super(repository, properties);
     }
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        TeeHttpServletResponse teeResponse = new TeeHttpServletResponse(response);
-
-        filterChain.doFilter(request, teeResponse);
-
-        teeResponse.finish();
-
-        request.setAttribute("responseBody", teeResponse.getOutputBuffer());
-
-        super.doFilterInternal(request, teeResponse, filterChain);
+    private String getRequestBody(ContentCachingRequestWrapper request) {
+        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        String characterEncoding = wrapper.getCharacterEncoding();
+        return getPayload(wrapper.getContentAsByteArray(), characterEncoding);
     }
 
     @Override
     protected Map<String, Object> getTrace(HttpServletRequest request) {
         Map<String, Object> trace = super.getTrace(request);
-
-        byte[] outputBuffer = (byte[]) request.getAttribute("responseBody");
-
-        if (outputBuffer != null) {
-            trace.put("responseBody", new String(outputBuffer));
+        Object requestBody = request.getAttribute(REQUEST_BODY);
+        Object responseBody = request.getAttribute(RESPONSE_BODY);
+        if (requestBody != null) {
+            trace.put(REQUEST_BODY, requestBody);
         }
-
+        if (responseBody != null) {
+            trace.put(RESPONSE_BODY, responseBody);
+        }
         return trace;
     }
+
+    public String getPayload(byte[] buf, String characterEncoding) {
+        String payload = null;
+        if (buf.length > 0) {
+            try {
+                payload = new String(buf, 0, buf.length, characterEncoding);
+            } catch (UnsupportedEncodingException ex) {
+                payload = "[unknown]";
+            }
+        }
+        return payload;
+    }
+
+    private String getResponseBody(ContentCachingResponseWrapper response) {
+        ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response,
+                ContentCachingResponseWrapper.class);
+        return getPayload(wrapper.getContentAsByteArray(), wrapper.getCharacterEncoding());
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        responseWrapper.copyBodyToResponse();
+        request.setAttribute(REQUEST_BODY, getRequestBody(requestWrapper));
+        request.setAttribute(RESPONSE_BODY, getResponseBody(responseWrapper));
+        super.doFilterInternal(requestWrapper, responseWrapper, filterChain);
+    }
+
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//            throws ServletException, IOException {
+//        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+//        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+//
+//        TeeHttpServletResponse teeResponse = new TeeHttpServletResponse(responseWrapper);
+//
+//        filterChain.doFilter(responseWrapper, responseWrapper);
+//
+//        teeResponse.finish();
+//
+//        request.setAttribute("responseBody", teeResponse.getOutputBuffer());
+//
+//        super.doFilterInternal(request, teeResponse, filterChain);
+//    }
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//            throws ServletException, IOException {
+//        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+//        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+//        filterChain.doFilter(requestWrapper, responseWrapper);
+//        responseWrapper.copyBodyToResponse();
+//        request.setAttribute(REQUEST_BODY, getRequestBody(requestWrapper));
+//        request.setAttribute(RESPONSE_BODY, getResponseBody(responseWrapper));
+//        super.doFilterInternal(requestWrapper, responseWrapper, filterChain);
+//    }
+
+//    @Override
+//    protected Map<String, Object> getTrace(HttpServletRequest request) {
+//        Map<String, Object> trace = super.getTrace(request);
+//
+//        byte[] outputBuffer = (byte[]) request.getAttribute("responseBody");
+//
+//        if (outputBuffer != null) {
+//            trace.put("responseBody", new String(outputBuffer));
+//        }
+//
+//        return trace;
+//    }
 
     @Override
     protected boolean shouldNotFilter(final HttpServletRequest request) throws ServletException {
--- spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/TeeServletOutputStream.java
+++ spring-boot-actuator-customization/src/main/java/com/sedooe/actuator/config/TeeServletOutputStream.java
@@ -1,3 +1,16 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
 package com.sedooe.actuator.config;
 
 import java.io.ByteArrayOutputStream;
@@ -14,14 +27,14 @@ public class TeeServletOutputStream extends ServletOutputStream {
 
     TeeServletOutputStream(ServletResponse httpServletResponse) throws IOException {
         // System.out.println("TeeServletOutputStream.constructor() called");
-        underlyingStream = httpServletResponse.getOutputStream();
+        this.underlyingStream = httpServletResponse.getOutputStream();
         baosCopy = new ByteArrayOutputStream();
     }
 
     byte[] getOutputStreamAsByteArray() {
         return baosCopy.toByteArray();
     }
-
+ 
     @Override
     public void write(int val) throws IOException {
         if (underlyingStream != null) {
